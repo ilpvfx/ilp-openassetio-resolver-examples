3c0da6c56c494617645c0b2bc12aeeefeee6dcd6
Ludvig Holmkvist <ludvig.holmkvist@ilpvfx.com>
2024-09-14 16:26:03 +0200

    Maya: Init. resolver with default manager and uri scheme.

--- maya/plug-ins/OpenAssetIOMayaResolver.py
+++ maya/plug-ins/OpenAssetIOMayaResolver.py
@@ -1,14 +1,29 @@
 from __future__ import annotations
 
 import weakref
+from functools import partial
 
 import maya.OpenMaya as OpenMaya
 import maya.OpenMayaMPx as OpenMayaMPx
-from openassetio.hostApi import HostInterface
-from openassetio.log import LoggerInterface
+from openassetio import constants, errors
+from openassetio.hostApi import HostInterface, Manager, ManagerFactory
+from openassetio.log import LoggerInterface, SeverityFilter
+from openassetio.pluginSystem import (
+    CppPluginSystemManagerImplementationFactory,
+    HybridPluginSystemManagerImplementationFactory,
+    PythonPluginSystemManagerImplementationFactory,
+)
 
 
 class OpenAssetIOResolver(OpenMayaMPx.MPxFileResolver):
+    def __init__(self, manager: Manager, uriScheme: str):
+        super().__init__()
+
+        self._manager = manager
+        self._context = manager.createContext()
+
+        self._uriScheme = uriScheme
+
     @staticmethod
     def className() -> str:
         """Returns the name of this class."""
@@ -16,19 +31,21 @@ class OpenAssetIOResolver(OpenMayaMPx.MPxFileResolver):
         return "OpenAssetIOResolver"
 
     @staticmethod
-    def theCreator() -> weakref.ProxyType[OpenAssetIOResolver]:
+    def theCreator(
+        manager: Manager, uriScheme: str
+    ) -> weakref.ProxyType[OpenAssetIOResolver]:
         """Returns a pointer to a function that will return a pointer to a new instance of
         this resolver.
         """
 
-        return OpenMayaMPx.asMPxPtr(OpenAssetIOResolver())
+        return OpenMayaMPx.asMPxPtr(OpenAssetIOResolver(manager, uriScheme))
 
     def uriScheme(self) -> str:
         """This method is called to query the URI scheme that is handled by this
         resolver.
         """
 
-        return "foo"
+        return self._uriScheme
 
     def resolveURIWithContext(
         self,
@@ -102,12 +119,67 @@ class MayaOpenAssetIOResolverLogger(LoggerInterface):
                 OpenMaya.MGlobal.displayWarning(message)
 
 
+def getDefaultManager() -> Manager | None:
+    """Creates and returns the default OpenAssetIO Manager instance using a hybrid
+    C++ and Python plugin system. This manager is used to interface with assets
+    in Maya through OpenAssetIO.
+    """
+
+    logger = SeverityFilter(MayaOpenAssetIOResolverLogger())
+
+    # Create a hybrid plugin system that combines both C++ and Python plugin systems.
+    # This allows the manager to load and interact with plugins from both languages.
+    factory = HybridPluginSystemManagerImplementationFactory(
+        [
+            CppPluginSystemManagerImplementationFactory(logger),
+            PythonPluginSystemManagerImplementationFactory(logger),
+        ],
+        logger,
+    )
+
+    # Create the host interface, which identifies this system (Maya) to the manager.
+    host = MayaOpenAssetIOResolverHost()
+
+    return ManagerFactory.defaultManagerForInterface(host, factory, logger)
+
+
 def initializePlugin(plugin: OpenMaya.MObject):
+    """Initializes the plugin in Maya by registering a URI-based file resolver.
+
+    This function checks for the availability of a default OpenAssetIO manager and
+    verifies that the manager has the necessary resolution capabilities. It also
+    retrieves the entity reference prefix from the manager's info and uses it to
+    register the URI file resolver.
+    """
+
     pluginFn = OpenMayaMPx.MFnPlugin(plugin)
 
     try:
+        if not (manager := getDefaultManager()):
+            raise errors.ConfigurationException(
+                "No default OpenAssetIO manager configured"
+            )
+
+        if not manager.hasCapability(manager.Capability.kResolution):
+            raise errors.ConfigurationException(
+                f"Entity resolution not supported by manager: {manager.displayName()!r}"
+            )
+
+        # Retrieve the entity reference prefix from the manager's info.
+        prefix = manager.info().get(constants.kInfoKey_EntityReferencesMatchPrefix)
+        if not prefix:
+            raise errors.ConfigurationException(
+                f"Unable to retrieve prefix for manager: {manager.displayName()!r}"
+            )
+
+        # Derive the URI scheme from the prefix, defaulting to the prefix if no scheme is
+        # found and ensure that any protocol-like components (e.g., "://") are excluded.
+        uriScheme = OpenMaya.MURI(prefix).getScheme() or prefix
+
         pluginFn.registerURIFileResolver(
-            OpenAssetIOResolver.className(), "foo", OpenAssetIOResolver.theCreator
+            OpenAssetIOResolver.className(),
+            uriScheme,
+            partial(OpenAssetIOResolver.theCreator, manager, uriScheme),
         )
 
     except Exception:
